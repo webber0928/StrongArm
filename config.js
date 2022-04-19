1611b4c42b4c7c569c419022cdaf25bc8a28cca8
WebberWu <webber0928@users.noreply.github.com>
2022-04-20 00:53:32 +0800

    add crawler path

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    team: 'https://basketball.biji.co/index.php?q=tools&act=team-info&id=3354&tab=stats&league=all',
+    game: 'https://basketball.biji.co/index.php?q=tools&act=game-result&id=13426',
+    player: 'https://basketball.biji.co/index.php?q=tools&act=player-info&id=34159'
+};
