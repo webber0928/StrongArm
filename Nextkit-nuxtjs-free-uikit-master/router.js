4d8b094021624baf70116c3ab3b6e92f6b68d2dc
Webber Woo <bird23074035@gmail.com>
2022-05-07 17:35:20 +0800

    add router buildModules

---
+++ Nextkit-nuxtjs-free-uikit-master/router.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+// 解決vue-router 3.1.0版後的重複點極路由報錯bug
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        name: '首頁',
+        component: () => import("@/pages/index").then(m => m.default || m),
+        // component: () => import("@/pages/home/index").then(m => m.default || m),
+      },
+      // {
+      //   path: '/player/:id',
+      //   name: '球員資料',
+      //   component: () => import("@/pages/player/_id").then(m => m.default || m)
+      // },
+      // {
+      //   path: '/team',
+      //   name: '球隊介紹',
+      //   component: () => import("@/pages/teamInfo/index").then(m => m.default || m)
+      // },
+      // {
+      //   path: '/team/pref',
+      //   name: '球員數據',
+      //   component: () => import("@/pages/teamPref/index").then(m => m.default || m),
+      // },
+      // {
+      //   path: '/404',
+      //   name: '404',
+      //   component:  () => import("@/pages/systemPage/404").then(m => m.default || m),
+      // },
+      // {
+      //   path: '*',
+      //   name: '404 not Found',
+      //   redirect: '/404'
+      // }
+    ]
+  })
+}
